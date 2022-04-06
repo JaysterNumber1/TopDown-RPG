12b4e954b492874019b26a89eb8927a3d782e485
Jarrett <jaysternumber1@gmail.com>
2022-04-06 09:43:30 -0600

    Added Player HP & MP Bars

---
+++ Assets/Scripts/Battle/Units.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Units : MonoBehaviour
+{
+    public string unitName;
+
+    public int level;
+
+    public int strength;
+    public int defense;
+
+    public int maxHP;
+    public int currentHP;
+
+    public int maxMP;
+    public int currentMP;
+    
+
+
+}
