39ba592a3880d939c37342df882421ea265760e9
Damion <dr11652@ms.wasatch.edu>
2022-04-08 08:42:21 -0600

    Gutted XP/Level System
    gutted for further development later, focusing more on loot drops right now.

--- Assets/DamionCode/XpSystem.cs
+++ Assets/DamionCode/XpSystem.cs
@@ -8,10 +8,8 @@ public class XpSystem : MonoBehaviour
 
     public int level;
     public float currentExp;
-    public float requiredXp;
-
-    private float lerpTimer;
-    private float delayTimer;
+    public float requiredEp;
+    public float maxExp;
 
     public Image xpBarFront;
     public Image xpBarBack;
@@ -19,40 +17,18 @@ public class XpSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        xpBarFront.fillAmount = currentExp / requiredXp;
-        xpBarBack.fillAmount = currentExp / requiredXp;
+        xpBarFront.fillAmount = currentExp / maxExp;
+        xpBarBack.fillAmount = currentExp / maxExp;
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateXpUi();
-        if (Input.GetKeyDown(KeyCode.Equals))
-        {
-            GainExperienceFlatRate(20);
-        }
-    }
 
-    public void UpdateXpUi()
-    {
-        float xpFraction = currentExp / requiredXp;
-        float FXP = xpBarFront.fillAmount;
-        if (FXP < xpFraction)
-        {
-            delayTimer += Time.deltaTime;
-            xpBarBack.fillAmount = xpFraction;
-            if(delayTimer > 3)
-            {
-                lerpTimer += Time.deltaTime;
-                float percentComplete = lerpTimer / 4;
-                xpBarFront.fillAmount = Mathf.Lerp(FXP, xpBarBack.fillAmount, percentComplete);
-            }
-        }
     }
 
     public void GainExperienceFlatRate(float xpGained)
     {
         currentExp += xpGained;
-        lerpTimer = 0f;
     }
 }
