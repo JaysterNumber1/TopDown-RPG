3e1b37e6c94735cc0cae333ebd2be7cbde76c2bd
Jarrett <jaysternumber1@gmail.com>
2022-04-18 09:06:40 -0600

    trying to create icons now

---
+++ Assets/Scripts/World/IconGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public Camera camera;
+    void TakeScreenshot(string fullPath)
+    {
+        if (camera == null)
+        {
+            camera = GetComponent<Camera>();
+        }
+
+        RenderTexture rt = new RenderTexture(256, 256, 24);
+        camera.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+        camera.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, 256, 256), 0, 0);
+        camera.targetTexture = null;
+        RenderTexture.active = null;
+
+        if (Application.isEditor)
+        {
+            DestroyImmediate(rt);
+
+        }
+        else
+        {
+            Destroy(rt);
+        }
+
+        byte[] bytes = screenShot.EncodeToPNG();
+        System.IO.File.WriteAllBytes(fullPath, bytes);
+//#if UNITY_EDITOR
+//        AssetDatabase.Refresh();
+//#endif
+
+
+
+    }
+}
